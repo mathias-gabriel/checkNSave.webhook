6d028c16a3ce76853c26df17e4d3a0a5963553ac
Mathias Gabriel <mathias.gabriel@soat.fr>
2018-12-05 22:17:38 +0100

    mise à jour des intents

--- functions/index.js
+++ functions/index.js
@@ -1,172 +1,219 @@
-/**
- * Copyright 2017 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ChecknSave Le ChatBot Assistant Secouriste et Formateur Auteur Carlos Rodriguez  ver0.6 le 29.11.2018
- */
+// Copyright 2017, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the 'License');
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Dialogflow fulfillment getting started guide:
+// https://dialogflow.com/docs/how-tos/getting-started-fulfillment
+//  Auteurs  Mathias et Carlos  ver 0.5 le 05/12/18 
 
 'use strict';
 
+const http = require('http');
+const request = require("request");
+
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const {WebhookClient} = require('dialogflow-fulfillment');
-const ActionsSdkAssistant = require('actions-on-google').ActionsSdkAssistant;
-process.env.DEBUG = 'CHECKNSAVE:*'; // enables lib debugging statements
-admin.initializeApp(functions.config().firebase);
-const db = admin.firestore();
+const {
+	dialogflow,
+	Permission,
+	SimpleResponse,
+	BasicCard,
+	Image,
+	Suggestions,
+	Button,
+	actionsOnGoogle
+} = require('actions-on-google');
 
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
-  //.region('europe-west1')	
-  const agent = new WebhookClient({ request, response });
 
-function DEMANDER_AIDE_VICTIME_FONCTION (agent) {
-    // Get parameter from Dialogflow with the string to add to the database
-    const NomEntry = agent.parameters.NomEntry; //NomEntry
-	const TelEntry = agent.parameters.TelEntry; //TelEntry
-	const GpsEntry = agent.parameters.GpsEntry; //GpsEntry
-	//const NatureProblemeEntry = agent.parameters.NatureProblemeEntry; //NatureProblemeEntry
-    const dialogflowAgentRef = db.collection('CHECKNSAVE').doc('Victime');
-    return db.runTransaction(t => {
-      t.set(dialogflowAgentRef, {NOM: NomEntry,Tél: TelEntry,GPS: GpsEntry}); 
-      return Promise.resolve('Ecriture correctement effectuée');
-    }).then(doc => {
-     agent.add(`Merci et maintenant répondez par Oui ou par Non à mes questions`);
-	 //agent.tell('checklist');
-    }).catch(err => {
-      console.log(`Erreur d'écriture en base: ${err}`);
-      agent.add(`Erreur de mémorisation des données en base.`);
-    });
-  }
+// Create an app instance
+const app = dialogflow()
 
-function CHECKLIST_FONCTION (agent) {
-    // Get parameter from Dialogflow with the string to add to the database
-    const SaignementEntry = agent.parameters.SaignementEntry;
-    const EtouffementEntry = agent.parameters.EtouffementEntry;
-	const ConscienteEntry = agent.parameters.ConscienteEntry;
-	const RespireEntry = agent.parameters.RespireEntry;
-    const dialogflowAgentRef = db.collection('CHECKNSAVE').doc('Checklist');
-    return db.runTransaction(t => {
-      t.set(dialogflowAgentRef, {Saigenement: SaignementEntry,Etouffement: EtouffementEntry,Consciente: ConscienteEntry, Respire: RespireEntry}); 
-      return Promise.resolve('Ecriture correctement effectuée');
-    }).then(doc => {
-      //agent.add(`La checklist a bien été enregistrée.`);
-	  // à faire fonction Matrice  4 Q/ OUI/NON -----> DiagProbable -----> TitreFormation
-	  agent.add(`La victime nécessite de toute urgence une Réanimation Cardio Pulmonaire.Voulez-vous que je vous explique comment on fait une RCP? dites RCP`);
-	  //agent.tell('RCP'); 
-    }).catch(err => {
-      console.log(`Erreur d'écriture en base: ${err}`);
-      agent.add(`Erreur de mémorisation en base.`);
-    });
-  }
-  
-function DEMANDER_AIDE_TEMOIN_FONCTION (agent) {
-    // Get parameter from Dialogflow with the string to add to the database
-    const ModeEntry = agent.parameters.ModeEntry;
-    const dialogflowAgentRef = db.collection('CHECKNSAVE').doc('Témoin');
-    return db.runTransaction(t => {
-      t.set(dialogflowAgentRef, {Mode: ModeEntry}); //Mode Assistant-secouriste Mode Formateur
-      return Promise.resolve('Ecriture correctement effectuée');
-    }).then(doc => {
-      agent.add(`"${ModeEntry}"`);
-	  //agent.tell('formateur');
-    }).catch(err => {
-      console.log(`Erreur d'écriture en base: ${err}`);
-      agent.add(`Erreur de mémorisation du mode sélectionné en base.`); //"${databaseEntry}"
-    });
-  }
+/******************AUTORISATION ACTIVATION GEOLOCALISATION SUR DEVICE************/
+app.intent('REQUEST_PERMISSION', (conv) => {
+	
+	conv.data.requestedPermission = 'DEVICE_PRECISE_LOCATION';
+	return conv.ask(new Permission({
+		context: 'to locate you',
+		// localise moi
+		permissions: conv.data.requestedPermission,
+	}));
 
-function FORMATION_FONCTION (agent) {
-    // Get parameter from Dialogflow with the string to add to the database
-    const TitreFormationEntry  = agent.parameters.TitreFormationEntry ; //TitreFormationEntry 
-	//  Pour TitreFormationEntry   nous mettrons  ex  RCP_INTENT  + RCP_VIDEO_INTENT
-    const dialogflowAgentRef = db.collection('CHECKNSAVE').doc('Formation');
-    return db.runTransaction(t => {
-      t.set(dialogflowAgentRef, {TitreFormation: TitreFormationEntry }); 
-      return Promise.resolve('Ecriture correctement effectuée');
-    }).then(doc => {
-	  agent.add(`OK je vais vous montrer quels sont les gestes à faire pour "${TitreFormationEntry}" dites "${TitreFormationEntry}"`);
-	  //agent.tell(`"${TitreFormationEntry}"`);
-      //agent.tell('RCP');	  
-    }).catch(err => {
-      console.log(`Erreur d'écriture en base: ${err}`);
-      agent.add(`Erreur de mémorisation en base.`);
-    });
-  }
-  
-function RCP_FONCTION (agent) {
-    // Get parameter from Dialogflow with the string to add to the database
-    const DefibrillateurEntry = agent.parameters.DefibrillateurEntry; //DefibrillateurEntry
-	const Titre_VideoEntry = agent.parameters.Titre_VideoEntry; //Titre_VideoEntry
-	//  Pour RCP_INTENT  on pose la question s'il souhaite avoir le défibrillateur le plus proche et si OUI ----> on lui dit + afficahege map + le témoin nous demande la vidéo RCP_VIDEO_INTENT
-    const dialogflowAgentRef = db.collection('CHECKNSAVE').doc('Formation');
-    return db.runTransaction(t => {
-      t.set(dialogflowAgentRef, {Défibrillateur: DefibrillateurEntry,Titre: Titre_VideoEntry}); 
-      return Promise.resolve('Ecriture correctement effectuée');
-    }).then(doc => {
-	  agent.add(`Nous avons trouvé un défibrillateur à 200 mètres. Dites "${Titre_VideoEntry}" si vous souhaitez visualiser une vidéo de formation`);  //ex  RCP vidéo
-	  //agent.tell(`"${Titre_VideoEntry}"`);
-	  //agent.tell('vidéo RCP');	  
-    }).catch(err => {
-      console.log(`Erreur d'écriture en base: ${err}`);
-      agent.add(`Erreur de mémorisation en base.`);
-    });
-  }
-
-function RCP_VIDEO_FONCTION (agent) {
-    // Get parameter from Dialogflow with the string to add to the database
-    const Lien_videoEntry = agent.parameters.Lien_videoEntry; //https://www.youtube.com/watch?v=4W5X-9BQjlU
-	const Type_deviceEntry = agent.parameters.Type_deviceEntry; //  Chromecast ou Smarphone ?
-	//  Pour le lien de la vidéo RCP_VIDEO_INTENT  il faudra mettre  Btn  arrêt/pause/de lecture vidéo  commander avec la voix
-    const dialogflowAgentRef = db.collection('CHECKNSAVE').doc('Formation');
-    return db.runTransaction(t => {
-      t.set(dialogflowAgentRef, {Lien: Lien_videoEntry, Type: Type_deviceEntry}); 
-      return Promise.resolve('Ecriture correctement effectuée');
-    }).then(doc => {
-	  //agent.add(`Daccord j'ai mis "${Titre_VideoEntry}" sur votre "${Type_deviceEntry}"`);
-	  agent.add(`Daccord j'ai mis "${Lien_videoEntry}" sur votre "${Type_deviceEntry}"`);
-	  //agent.tell(`Le lien est "${Lien_videoEntry}"`); 
-    }).catch(err => {
-      console.log(`Erreur d'écriture en base: ${err}`);
-      agent.add(`Erreur de mémorisation en base.`);
-    });
-  }
-
-  // Map from Dialogflow intent names to functions to be run when the intent is matched
-  let intentMap = new Map();
-  //intentMap.set('PROTEGER_INTENT', PROTEGER_FONCTION); //$Risque_eventuelEntry  Il y a t-il des risques éventuels? Oui? ou Non?
-  intentMap.set('DEMANDER_AIDE_VICTIME_INTENT', DEMANDER_AIDE_VICTIME_FONCTION);
- //intentMap.set('GPS_INTENT', GPS_FONCTION); // GPS LatEntry LngEntry Lat=48.8938615,Lng=2.2777903
-  intentMap.set('CHECKLIST_INTENT', CHECKLIST_FONCTION);
-  //intentMap.set('NATURE_PROBLEME_INTENT', NATURE_PROBLEME_FONCTION);//La nature de votre problème est bien  $NatureProblemeEntry . Est-ce exacte Oui? ou Non?
-  //BILAN_INTENT
-  //intentMap.set('ALERTER_INTENT', ALERTER_FONCTION); // $BilanEntry oui non envoyé par SMS
-  intentMap.set('DEMANDER_AIDE_TEMOIN_INTENT', DEMANDER_AIDE_TEMOIN_FONCTION);
-  intentMap.set('FORMATION_INTENT', FORMATION_FONCTION); // TitreFormationEntry
-   //SAIGNEMENT_VIDEO_INTENT
-   //ETOUFFEMENT_VIDEO_INTENT
-   //CONSCIENTE_VIDEO_INTENT
-   //DEFIBRILLATEUR_VIDEO_INTENT
-//RESPIRE_VIDEO_INTENT
-//MALAISE_VIDEO_INTENT
-//PLAIE_VIDEO_INTENT
-//BRULURE_VIDEO_INTENT
-//TRAUMATISME_VIDEO_INTENT   
-  intentMap.set('RCP_INTENT', RCP_FONCTION); // DefibrillateurEntry Titre_VideoEntry  
-  intentMap.set('RCP_VIDEO_INTENT', RCP_VIDEO_FONCTION); //Lien_videoEntry  Type_deviceEntry
-  //intentMap.set('PLS_INTENT', PLS_FONCTION); // PLS_INTENT  FormationEntry
-  //PLS_VIDEO_INTENT
-  //REQUEST_PERMISSION @sys.date-period 
-  //USER_INFO   erreur de traitement de la demande de localisation
- // intentMap.set('INCONNUE_INTENT', INCONNUE_INTENT_FONCTION); // Default Fallback Intent  INCONNUE_INTENT  action = input.unknown
-  agent.handleRequest(intentMap);
 });
+
+
+/********************DEMANDER_AIDE_VICTIME_INTENT*********************************/
+app.intent('DEMANDER_AIDE_VICTIME_INTENT', (conv, params, permissionGranted) => {
+
+	//const NomEntry = params.NomEntry; //NomEntry
+	//const TelEntry = params.TelEntry; //TelEntry
+
+	if (permissionGranted) {
+		
+		const {requestedPermission} = conv.data;
+
+		if (requestedPermission === 'DEVICE_PRECISE_LOCATION') {
+			console.log(conv.device.location.coordinates.latitude);
+			var latitude = conv.device.location.coordinates.latitude;
+			console.log(conv.device.location.coordinates.longitude);
+			var longitude = conv.device.location.coordinates.longitude;
+
+			//return conv.close('vos informations ont été envoyé.');
+
+			return new Promise(function(resolve, reject){
+
+				var url = 'http://owaveservices.info:5003/send_report_by_email';
+				var requestData = {
+					"report": "string"
+				}
+
+				request({
+					url: url,
+					method: "POST",
+					json: requestData
+				}, function (err, response, body) {
+					// in addition to parsing the value, deal with possible errors
+					if (err) return reject(err);
+					try {
+						// JSON.parse() can throw an exception if not valid JSON
+						resolve(JSON.parse(body).data);
+					} catch(e) {
+						reject(e);
+					}
+				});
+			}).then(function(val) {
+
+				return conv.ask("En attendant les secours, quel est la nature de votre problème, c\'est un accident? ou une maladie?"); //coordinates.latitude
+
+			}).catch(function(err) {
+				console.log(err);
+				return conv.ask('vos informations n\'ont pas été envoyé.');
+			});
+
+
+		}else{
+			return conv.ask('je n\'ai pas pu récupéré votre position.');
+		}
+
+	} else {
+		return conv.ask('Sorry, permission denied.');
+	}
+
+
+});
+
+/******************BILAN A ENVOYER PAR MAIL AUX URGENCES************/
+app.intent('BILAN_INTENT', (conv, params) => {
+	
+// 	BILAN à écrire dans le Mail d'envoie au Urgences
+// 	Objet: Bilan d'un accident au 7 rue Paul Vaillant Couturier, 92300 Levallois-Perret 08/12/18 16h00
+//	Nom: Carlos Rodriguez
+//	Tél: 0624102987
+//	Nature du problème: Réanimation Cardio Pulmonaire (RCP) Accident cardiovasculaire dans un Hackathon 
+//	Risques éventuels: Aucun risques éventuels, ni sur-accident 
+//	Localisation précise de l'événement: 7 Rue Paul Vaillant Couturier, 92300 Levallois-Perret 
+//	Nombre de personnes concernées= 1 victime
+//	Etat de chaque victime= La victime est inconsciente et ne respire plus
+//	Premières mesures prises= Alerter les urgences + RCP (avec défibrillateur)
+//  http://owaveservices.info:5003/defibrilatteurs/48.8938615/2.2777903/0/1
+//  http://owaveservices.info:5003/send_report_by_email/Carlos/0624102987/48.8938615/2.2777903
+// http://crodriguez.free.fr/checknsave/ico/CheckList_66x66.png  
+// http://crodriguez.free.fr/checknsave/ico/Bilan_66x66.png
+// http://crodriguez.free.fr/checknsave/ico/ChatBot_66x66.png  
+// http://crodriguez.free.fr/checknsave/ico/Aide_66x66.png  
+    const NomEntry = params.NomEntry; //NomEntry
+	const TelEntry = params.TelEntry; //TelEntry
+	const LatEntry = params.LatEntry; //LatEntry 48.8938615
+	const LngEntry = params.LngEntry; //LngEntry  2.2777903
+	const AdresseEntry = params.AdresseEntry; //AdresseEntry 7 rue Paul Vaillant Couturier, 92300 Levallois-Perret
+	const NatureProblemeEntry = params.NatureProblemeEntry; //NatureProblemeEntry
+    const DateProblemEntry = params.DateProblemEntry;  //DateProblemEntry   08/12/18
+	const TimeProblemEntry = params.TimeProblemEntry;  //TimeProblemEntry  16h00
+	const RisqueEventuelEntry= 'Aucun risques éventuels, ni sur-accident';
+    const NbrVictimeEntry='1';
+	const EtatVictimeEntry='La victime est inconsciente et ne respire plus';
+	const PremieresMesuresEntry='Alerter les urgences + RCP (avec défibrillateur)';
+
+if ( !conv.surface.capabilities.has('actions.capability.SCREEN_OUTPUT') ) {
+		return conv.ask('Sorry, try this on a screen device or select the phone surface in the simulator.');
+	}else{
+
+		// Create a basic card
+		return conv.ask(new BasicCard({
+			formattedText: "BILAN",
+			subtitle: AdresseEntry,
+			title: NatureProblemeEntry,
+			buttons: new Button({
+				title: 'Envoyer Bilan',
+				url: '',
+			}),
+			image: new Image({
+				url: 'http://crodriguez.free.fr/checknsave/ico/Bilan_66x66.png',
+				alt: 'Image alternate text',
+			})
+		}));
+	
+	}
+	return conv.ask('Un bilan a été transmis par mail aux urgences');
+
+});
+
+
+/******************GEOLOCALISATION DEFIBRILLATEUR LE PLUS PROCHE D'UNE VICTIME************/
+app.intent('DEFIBRILLATEUR_INTENT', (conv, params, permissionGranted) => {
+
+	const DefibrillateurEntry = params.DefibrillateurEntry; //DefibrillateurEntry
+	const Titre_VideoEntry = params.Titre_VideoEntry; //Titre_VideoEntry
+
+	if (permissionGranted) {
+
+		const {requestedPermission} = conv.data;
+
+		if (requestedPermission === 'DEVICE_PRECISE_LOCATION') {
+			console.log(conv.device.location.coordinates.latitude);
+			const latitude = conv.device.location.coordinates.latitude;
+			console.log(conv.device.location.coordinates.longitude);
+			const longitude = conv.device.location.coordinates.longitude;
+
+			return new Promise(function(resolve, reject){
+//http://owaveservices.info:5003/defibrilatteurs/48.8938615/2.2777903/0/1
+				request('http://owaveservices.info:5003/defibrilatteurs/'+longitude+'/'+latitude+'/0/1', function (err, response, body) {
+					// in addition to parsing the value, deal with possible errors
+					if (err) return reject(err);
+					try {
+						// JSON.parse() can throw an exception if not valid JSON
+						resolve(JSON.parse(body));
+					} catch(e) {
+						reject(e);
+					}
+				});
+			}).then(function(val) {
+				console.log(val);
+				var distance = Math.floor( val[0].distance );
+				var message = 'Nous avons trouvé un défibrillateur à '+distance+' mètres.';
+				return conv.ask(message);
+
+			}).catch(function(err) {
+				console.log(err);
+				return conv.ask('Nous n\'avons pas pu trouvé de défibrillateur.');
+			});
+
+		}else{
+			return conv.ask('vos informations n\'ont pas été envoyé.');
+		}
+
+	} else {
+		return conv.ask('Sorry, permission denied.');
+	}
+
+});
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
+
